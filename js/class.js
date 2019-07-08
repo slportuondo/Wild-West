d8dbd952ed81946ba2d9c1bc149b73c493acba72
Sebastian Portuondo <slportuondo@gmail.com>
2019-07-08 15:46:09 -0500

    Individual players can be created

---
+++ js/class.js
@@ -0,0 +1,29 @@
+// Create a class that creates Player object
+  // Constructor method
+    // Player number
+    // Player ammo: 1
+    // Player shield: 3
+    // Player vulnerability: false while the player has shields
+    // Player keypress: '';
+  // Function to let player shoot
+  // Function to let player reload
+  // Function to let player shield
+
+class Player {
+  constructor(number){
+    this.player = 'player' + number;
+    this.ammo = 1;
+    this.shield = 3;
+    this.vulnerability = false;
+    this.action = '';
+  }
+  shoot(enemy){
+    enemy.shield --;
+  }
+  reload(){
+    this.ammo ++;
+  }
+  shield() {
+    this.shield --;
+  }
+}
