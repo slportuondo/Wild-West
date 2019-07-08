fc1b3a026f5dae6209aedec2ade7048fa5f9260d
Sebastian Portuondo <slportuondo@gmail.com>
2019-07-08 16:51:46 -0500

    Created working countdown

--- js/app.js
+++ js/app.js
@@ -1,21 +1,42 @@
 console.log('hello world');
-// Game object
-  // Player 1 object
-  // Player 2 object
-  // Timer in seconds: 3, 2, 1, 0  --> (0 = go)
-  // Function to start game
-  // Funtion to create timer that stops after 3 seconds
-  // Function to store keypress, if within the time limit
+  // Function to check keypress, if within the time limit
+    // Player 1 choose to shoot
+    // Player 1 choose to reload
+    // Player 1 choose to shield
+    // Player 2 choose to shoot
+    // Player 2 choose to shoot
+    // Player 2 choose to shield
+  // Function to clear keypresses at the end of a match
   // Function to increase points
   // Funtion to increase round number
+const game = {
+  players: [],   // Array of player objects
+  timer: null,   // Timer in seconds: 3, 2, 1, 0  --> (0 = go)
+  startGame() {
 
+  },
+  startCountdown() {
+    this.timer = 3;
+    // Funtion to create timer that stops after 3 seconds
+    let countdown = setInterval(() => {
+      $('#timer').text(this.timer);
+      if (typeof(this.timer) === 'number'){
+        this.timer--;
+      }
 
-
-
+      if (this.timer === 0) {
+        this.timer = '';
+      } else if (this.timer === ''){
+        clearInterval(countdown);
+      }
+    }, 1000);
+  },
+  playerAction(){}
+}
+game.startCountdown();
 
   // Event listeners that listen for keypress
-    // Player 1 choose shoot
-    // Player 1 choose reload
-    // Player 1 choose to shield
-    // Player 2 choose shoot
-    // Player 2 choose to shield
+    $(document).on('keypress', (e) => {
+      console.log(e);
+      game.startGame(e);
+    });
