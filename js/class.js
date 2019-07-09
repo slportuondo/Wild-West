be06240824bbe84b9ba6dc6bbeede126a42db4da
Sebastian Portuondo <slportuondo@gmail.com>
2019-07-08 21:01:39 -0500

    Edited Player class

--- js/class.js
+++ js/class.js
@@ -1,32 +1,37 @@
-// Create a class that creates Player object
-  // Constructor method
-    // Player number
-    // Player ammo: 1
-    // Player shield: 3
-    // Player vulnerability: false while the player has shields
-    // Player keypress: '';
-  // Function to let player shoot
-  // Function to let player reload
-  // Function to let player shield
-
 class Player {
   constructor(number){
     this.player = 'player' + number;
-    this.ammo = 1;
-    this.shield = 3;
+    this.alive = true;
     this.vulnerability = false;
+    this.shield = [3, false]; //Indicates number of shields, whether or not shield was used last turn
+    this.ammo = 1;
     this.action = '';
   }
   shoot(enemy){
-    enemy.shield --;
     this.vulnerability = true;
+    this.ammo--;
+    if (enemy.vulnerability === true) {
+      enemy.alive = false;
+    }
   }
   reload(){
-    this.ammo ++;
     this.vulnerability = true;
+    this.shield[1] = false;
+    if (this.ammo < 2) {
+      this.ammo ++;
+    }
   }
   shield() {
-    this.shield --;
-    this.vulnerability = false;
+    // Reload shields if they weren't used last turn
+    if (this.shield[1] === false){
+      this.shield[0] = 3;
+    }
+    this.shield[1] = true;
+    if (this.shield[0] > 0) {
+      this.shield[0] --;
+      this.vulnerability = false;
+    } else {
+      this.vulnerability = true;
+    }
   }
 }
